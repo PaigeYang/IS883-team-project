5cd2f9afae4e9b611dc67662330dc95a764e039e
PaigeYang <33799289+PaigeYang@users.noreply.github.com>
2024-11-30 21:22:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,28 +172,10 @@ summary_type = st.selectbox(
 search_and_summarize_restaurants(user_query, store_type, summary_type)
 
 
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
+pip install streamlit-geolocation
 
-loc_button = Button(label="Get Location")
-loc_button.js_on_event("button_click", CustomJS(code="""
-    navigator.geolocation.getCurrentPosition(
-        (loc) => {
-            document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
-        }
-    )
-    """))
-result = streamlit_bokeh_events(
-    loc_button,
-    events="GET_LOCATION",
-    key="get_location",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
-
-if result:
-    if "GET_LOCATION" in result:
-        st.write(result.get("GET_LOCATION"))
+from streamlit_geolocation import streamlit_geolocation
+location = streamlit_geolocation()
+st.write(location)
 
 
